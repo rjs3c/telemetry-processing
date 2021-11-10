39bdb73382a166cdc980eef8fec5815050542830
rjs3c <78426161+rjs3c@users.noreply.github.com>
2021-11-10 16:55:37 +0000

    Update bootstrap.php

--- telemetry_processing_private/bootstrap.php
+++ telemetry_processing_private/bootstrap.php
@@ -11,23 +11,23 @@
  * @author Ryan Instrell
  */
 
-/* PSR-4 Autoloading */
+/** <PSR-4> Autoloading using <Composer>. */
 require 'vendor/autoload.php';
 
-/* Points to relative /app/ directory */
+/** Points to relative /app/ directory. */
 $app_path = __DIR__ . '/app/';
 
-/* Settings File - Global Constants and Configurations */
+/** Settings File - Global Constants and Configurations */
 $settings = require $app_path . 'settings.php';
 
-/* Dependencies File - Using DIC Containers for different classes */
+/** Dependencies File - Using DIC Containers for different classes */
 require $app_path . 'dependencies.php';
 
-/* Instantiation of Slim + DIC */
+/** Instantiation of Slim + DIC */
 $container = new \Slim\Container($settings);
 $app = new \Slim\App($container);
 
-/* Routes File - provides different routes */
+/** Routes File - provides different routes */
 require $app_path . 'routes.php';
 
 $app->run();
