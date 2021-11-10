8548df2bbb07e70ed195a78712e5ee53e02192c4
rjs3c <78426161+rjs3c@users.noreply.github.com>
2021-11-10 16:43:56 +0000

    Update settings.php

--- telemetry_processing_private/app/settings.php
+++ telemetry_processing_private/app/settings.php
@@ -11,57 +11,57 @@
  * @author Ryan Instrell
  */
 
-/* Ini Configurations and XDebug */
+/** Ini Configurations and XDebug. **/
 ini_set('display_errors', 'On');
 ini_set('html_errors', 'On');
 ini_set('xdebug.trace_output_name', 'telemetry_processing.%t');
 ini_set('xdebug.trace_format', '1');
 
-/* Paths */
+/** Paths needed for <CSS>, <Twig> and <Monolog>. **/
 $css_file_name = ''; // CSS file name needed here.
 $css_path = dirname($_SERVER['SCRIPT_NAME'] . '/css/' . $css_file_name);
 $template_path = __DIR__ . '/templates/';
 $log_path = '/p3t/phpappfolder/logs/';
 
-/* Settings */
-$telemetry_settings = array(
-    'displayErrorDetails' => true,
-    'addContentLengthHeader' => false,
-    'mode' => 'development',
-    'debug' => true,
-    'telemetryView' => array(
-        'twig_attributes' => array(
-            'cache' => false,
-            'auto_reload' => true,
-        )
-    ),
-    'databaseSettings' => array(
-        'rdbms' => '',
-        'db_host' => '',
-        'db_port' => '',
-        'db_name' => '',
-        'db_user_name' => '',
-        'db_user_password' => '',
-        'db_encoding' => 'utf8',
-        'db_collation' => 'utf8_unicode_ci',
-        'pdo_attributes' => array(
-            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES   => true,
-        )
-    ),
-    'soapSettings' => array(
-        'wsdl' => 'https://m2mconnect.ee.co.uk/orange-soap/services/MessageServiceByCountry?wsdl',
-        'soap_attributes' => array(
-            'trace' => true,
-            'exceptions' => true
-        )
-    )
-);
-
-/* Constants */
+/** Constants for <CSS>, <Twig> and <Monolog> paths. **/
 define('CSS_PATH', $css_path);
 define('TEMPLATE_PATH', $template_path);
 define('LOG_PATH', $log_path);
 
-return $telemetry_settings;
+/** Settings for <Twig> (telemetryView), <PDO> (databaseSettings) and <SOAPClient> (soapSettings). **/
+return array(
+    'telemetry_settings' => array(
+        'displayErrorDetails' => true,
+        'addContentLengthHeader' => false,
+        'mode' => 'development',
+        'debug' => true,
+        'telemetryView' => array(
+            'twig_attributes' => array(
+                'cache' => false,
+                'auto_reload' => true,
+            )
+        ),
+        'databaseSettings' => array(
+            'rdbms' => '',
+            'db_host' => '',
+            'db_port' => '',
+            'db_name' => '',
+            'db_user_name' => '',
+            'db_user_password' => '',
+            'db_encoding' => 'utf8',
+            'db_collation' => 'utf8_unicode_ci',
+            'pdo_attributes' => array(
+                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES   => true,
+            )
+        ),
+        'soapSettings' => array(
+            'wsdl' => 'https://m2mconnect.ee.co.uk/orange-soap/services/MessageServiceByCountry?wsdl',
+            'soap_attributes' => array(
+                'trace' => true,
+                'exceptions' => true
+            )
+        )
+    )
+);
