4865065f8d6dbcbe593f91df3d73a9951e8ccf9f
rjs3c <78426161+rjs3c@users.noreply.github.com>
2021-11-11 16:17:17 +0000

    Add files via upload

--- telemetry_processing_private/bootstrap.php
+++ telemetry_processing_private/bootstrap.php
@@ -17,17 +17,19 @@ require 'vendor/autoload.php';
 /** Points to relative /app/ directory. */
 $app_path = __DIR__ . '/app/';
 
-/** Settings File - Global Constants and Configurations */
+/** Settings File - Global Constants and Configurations. */
 $settings = require $app_path . 'settings.php';
 
-/** Dependencies File - Using DIC Containers for different classes */
+/** Instantiation of Slim DIC. */
+$container = new \Slim\Container($settings);
+
+/** Dependencies File - Using DIC Containers for different classes. */
 require $app_path . 'dependencies.php';
 
-/** Instantiation of Slim + DIC */
-$container = new \Slim\Container($settings);
+/** Instantiation of Slim App. */
 $app = new \Slim\App($container);
 
-/** Routes File - provides different routes */
+/** Routes File - provides different routes. */
 require $app_path . 'routes.php';
 
 $app->run();
