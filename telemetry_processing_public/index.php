0abc773288108c3b08c16a4f4b86ee688370dd88
rjs3c <78426161+rjs3c@users.noreply.github.com>
2021-11-15 08:18:39 +0000

    Add files via upload

--- telemetry_processing_public/index.php
+++ telemetry_processing_public/index.php
@@ -11,20 +11,4 @@
  * @author Ryan Instrell
  */
 
-/** Debugging and Ini Parameters. */
-ini_set('xdebug.trace_output_name', 'telemetry_processing');
-ini_set('display_errors', 'On');
-ini_set('html_errors', 'On');
-ini_set('xdebug.trace_format', 1);
-
-if (function_exists(xdebug_start_trace()))
-{
-    xdebug_start_trace();
-}
-
 include 'telemetry_processing/bootstrap.php';
-
-if (function_exists(xdebug_stop_trace()))
-{
-    xdebug_stop_trace();
-}
