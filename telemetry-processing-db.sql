b36ad4d8b0e7e8dfa030a0e23586b811358d3ad9
rjs3c <78426161+rjs3c@users.noreply.github.com>
2021-11-16 16:26:10 +0000

    Update telemetry-processing-db.sql

--- telemetry-processing-db.sql
+++ telemetry-processing-db.sql
@@ -1,1 +1,23 @@
+CREATE DATABASE IF NOT EXISTS `telemetry_db` COLLATE 'utf8_unicode_ci';
+CREATE USER 'user'@localhost IDENTIFIED BY ‘user_pass';
+GRANT SELECT, INSERT ON telemetry_db.* TO 'user'@'localhost';
 
+USE telemetry_db;
+DROP TABLE IF EXISTS `telemetry_data`;
+
+CREATE TABLE telemetry_data (
+ `sender_name` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
+ `sender_number` INT COLLATE utf8_unicode_ci NOT NULL,
+ `sender_email` varchar(320) COLLATE utf8_unicode_ci NOT NULL,
+ `sender_group` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
+ `switch_one` BIT COLLATE utf8_unicode_ci NOT NULL,
+ `switch_two` BIT COLLATE utf8_unicode_ci NOT NULL,
+ `switch_three` BIT COLLATE utf8_unicode_ci NOT NULL,
+ `switch_four` BIT COLLATE utf8_unicode_ci NOT NULL,
+ `fan` BIT COLLATE utf8_unicode_ci NOT NULL,
+ `temperature` DECIMAL(10,2) COLLATE utf8_unicode_ci NOT NULL,
+ `keypad` INT(1) COLLATE utf8_unicode_ci NOT NULL,
+ `timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON
+UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
+COMMENT='CURRENT_TIMESTAMP';
