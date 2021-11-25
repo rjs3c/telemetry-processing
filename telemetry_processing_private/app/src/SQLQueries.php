dadcb0710903a3723b7a64018ff59568dac3798e
rjs3c <78426161+rjs3c@users.noreply.github.com>
2021-11-25 20:14:28 +0000

    Add files via upload

--- telemetry_processing_private/app/src/SQLQueries.php
+++ telemetry_processing_private/app/src/SQLQueries.php
@@ -27,20 +27,20 @@ class SQLQueries
      */
     public static function insertTelemetryData() : string
     {
-        $query_string  = "INSERT INTO telemetry_data ";
-        $query_string .= "SET sender_name = :sender_name, ";
-        $query_string .= "sender_number = :sender_number, ";
-        $query_string .= "sender_email = :sender_email, ";
-        $query_string .= "sender_group = :sender_group, ";
-        $query_string .= "switch_one = :switch_one, ";
-        $query_string .= "switch_two = :switch_two, ";
-        $query_string .= "switch_three = :switch_three, ";
-        $query_string .= "switch_four = :switch_four, ";
-        $query_string .= "fan = :fan, ";
-        $query_string .= "temperature = :temperature, ";
-        $query_string .= "keypad = :keypad";
+        $sql_query  = "INSERT INTO telemetry_data ";
+        $sql_query .= "SET sender_name = :sender_name, ";
+        $sql_query .= "sender_number = :sender_number, ";
+        $sql_query .= "sender_email = :sender_email, ";
+        $sql_query .= "sender_group = :sender_group, ";
+        $sql_query .= "switch_one = :switch_one, ";
+        $sql_query .= "switch_two = :switch_two, ";
+        $sql_query .= "switch_three = :switch_three, ";
+        $sql_query .= "switch_four = :switch_four, ";
+        $sql_query .= "fan = :fan, ";
+        $sql_query .= "temperature = :temperature, ";
+        $sql_query .= "keypad = :keypad";
 
-        return $query_string;
+        return $sql_query;
     }
 
     /**
@@ -50,13 +50,13 @@ class SQLQueries
      */
     public static function getTelemetryData() : string
     {
-        $query_string  = "SELECT * ";
-        $query_string .= "FROM telemetry_db ";
-        // $query_string .= "WHERE session_id = :local_session_id ";
-        // $query_string .= "AND session_var_name = :session_var_name";
+        $sql_query  = "SELECT * ";
+        $sql_query .= "FROM telemetry_db ";
+        // $sql_query .= "WHERE session_id = :local_session_id ";
+        // $sql_query .= "AND session_var_name = :session_var_name";
 
         //To be updated when needed depending on what we choose to display to the user
 
-        return $query_string;
+        return $sql_query;
     }
 }
