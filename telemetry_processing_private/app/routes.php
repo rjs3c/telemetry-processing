8214c55a52012e0fb954775c7ae06556234abc38
rjs3c <78426161+rjs3c@users.noreply.github.com>
2021-11-29 11:08:59 +0000

    Add files via upload

--- telemetry_processing_private/app/routes.php
+++ telemetry_processing_private/app/routes.php
@@ -1,1 +1,18 @@
 <?php
+/**
+ * routes.php
+ *
+ * Contains all application routes.
+ *
+ * @package telemetry_processing
+ *
+ * @author James Brass
+ * @author Mo Aziz
+ * @author Ryan Instrell
+ */
+
+/* Homepage */
+//require 'routes/homepage.php';
+
+/* Testing route (DELETE IN PRODUCTION) */
+require 'routes/testfetchtelemetrydata.php';
