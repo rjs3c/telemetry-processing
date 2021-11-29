d198df76c0b0d1e3863df0d9dac62b583dda4cd4
rjs3c <78426161+rjs3c@users.noreply.github.com>
2021-11-29 10:40:46 +0000

    Update homepage.php

--- telemetry_processing_private/app/routes/homepage.php
+++ telemetry_processing_private/app/routes/homepage.php
@@ -10,3 +10,25 @@
  * @author Mo Aziz
  * @author Ryan Instrell
  */
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+$app->get('/', function(Request $request, Response $response) use ($app) {
+    $html_output = $this->view->render($response,
+        'homepage.html.twig',
+        array(
+            'page_title' => APP_TITLE
+        )
+    );
+
+    return gzipCompress($app, $html_output);
+})->setName('homepage');
+
+function gzipCompress($app, $html_output)
+{
+    $gzip_wrapper = $app->getContainer()->get('gzipWrapper');
+    $gzip_wrapper->setHtmlOutput($html_output);
+    $gzip_wrapper->gzipCompress();
+    return $gzip_wrapper->getCompressionOutput();
+}
