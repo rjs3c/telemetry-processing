df9bd429fed70acd2cdd5674bedfdeab5eb0362d
rjs3c <78426161+rjs3c@users.noreply.github.com>
2021-12-06 08:56:12 +0000

    Update TelemetryParserTest.php

--- telemetry_processing_private/tests/TelemetryParserTest.php
+++ telemetry_processing_private/tests/TelemetryParserTest.php
@@ -1,25 +1,45 @@
 <?php declare(strict_types=1);
+/**
+ * TelemetryParserTest.php
+ *
+ * Tests functionality for TelemetryParser.
+ * Tests:
+ * - Tests that, among numerous non-group specific messages, that the group-specific message can be correctly extracted.
+ * - Tests that, if no group-specific messages exist amidst many others, an empty result is returned.
+ *
+ * @package telemetry_processing
+ * @\TelemProc
+ *
+ * @author James Brass
+ * @author Mo Aziz
+ * @author Ryan Instrell
+ */
 
 use PHPUnit\Framework\TestCase;
 use TelemProc\TelemetryParser;
 
 final class TelemetryParserTest extends TestCase
 {
+    /**
+     * Test case to ensure that in a collection of non-group specific messages, that a group-specific message can be extracted correctly.
+     */
     public function testReturnsGroupXML() {
         $telemetry_parser = new TelemetryParser();
 
+        /* Message at index 1 is the correct message. The others are incorrect permutations of this. */
         $xml_messages = array(
-            '<messagerx><sourcemsisdn>447817814149</sourcemsisdn><destinationmsisdn>447817814149</destinationmsisdn><receivedtime>25/11/2021 11:44:11</receivedtime><bearer>SMS</bearer><messageref>0</messageref><message><Metadata><GID>INVALID_GROUP</GID></Metadata><Content><SW><SW1>1</SW1><SW2>1</SW2><SW3>0</SW3><SW4>1</SW4></SW><FN>forward</FN><TMP>34.0</TMP><KP>5</KP></Content></message></messagerx>',
-            '<messagerx><sourcemsisdn>447817814149</sourcemsisdn><destinationmsisdn>447817814149</destinationmsisdn><receivedtime>25/11/2021 11:44:11</receivedtime><bearer>SMS</bearer><messageref>0</messageref><message><Metadata><GID>AF</GID></Metadata><Content><SW><SW1>1</SW1><SW2>1</SW2><SW3>0</SW3><SW4>1</SW4></SW><FN>forward</FN><TMP>34.0</TMP><KP>5</KP></Content></message></messagerx>',
-            '<messagerx><sourcemsisdn>447817814149</sourcemsisdn><destinationmsisdn>447817814149</destinationmsisdn><receivedtime>25/11/2021 11:44:11</receivedtime><bearer>SMS</bearer><messageref>0</messageref><message>Invalid Message</message></messagerx>'
+            '<messagerx><sourcemsisdn>440000000000</sourcemsisdn><destinationmsisdn>440000000000</destinationmsisdn><receivedtime>25/11/2021 11:44:11</receivedtime><bearer>SMS</bearer><messageref>0</messageref><message><Content><GID>INVALID</GID><MSDN>+440000000000</MSDN><SW><SW1>1</SW1><SW2>1</SW2><SW3>0</SW3><SW4>1</SW4></SW><FN>forward</FN><TMP>34.0</TMP><KP>5</KP></Content></message></messagerx>',
+            '<messagerx><sourcemsisdn>440000000000</sourcemsisdn><destinationmsisdn>440000000000</destinationmsisdn><receivedtime>25/11/2021 11:44:11</receivedtime><bearer>SMS</bearer><messageref>0</messageref><message><Content><GID>AF</GID><MSDN>+440000000000</MSDN><SW><SW1>1</SW1><SW2>1</SW2><SW3>0</SW3><SW4>1</SW4></SW><FN>forward</FN><TMP>34.0</TMP><KP>5</KP></Content></message></messagerx>',
+            '<messagerx><sourcemsisdn>440000000000</sourcemsisdn><destinationmsisdn>440000000000</destinationmsisdn><receivedtime>25/11/2021 11:44:11</receivedtime><bearer>SMS</bearer><messageref>0</messageref><message>Invalid Message</message></messagerx>',
+            '<messagerx><sourcemsisdn>440000000000</sourcemsisdn><destinationmsisdn>440000000000</destinationmsisdn><receivedtime>25/11/2021 11:44:11</receivedtime><bearer>SMS</bearer><messageref>0</messageref><message><Content><MSDN>+440000000000</MSDN><SW><SW1>1</SW1><SW2>1</SW2><SW3>0</SW3><SW4>1</SW4></SW><FN>forward</FN><TMP>34.0</TMP><KP>5</KP></Content></message></messagerx>',
         );
 
+        /* Parsed deconstruction of group-specific message. */
         $expected_array = array(
             0 => array (
-                'Metadata' => array(
-                    'GID' => 'AF'
-                ),
                 'Content' => array(
+                    'GID' => 'AF',
+                    'MSDN' => '+440000000000',
                     'SW' => array(
                         'SW1' => '1',
                         'SW2' => '1',
@@ -38,6 +58,28 @@ final class TelemetryParserTest extends TestCase
 
         $this->assertEquals(
             $expected_array,
+            $telemetry_parser->getTelemetryParseResults()
+        );
+    }
+
+    /**
+     * Test case to identify if no provided XML messages meet the criteria to be classified as a group-specific message, nothing is returned (that is, empty result).
+     */
+    public function testReturnsNoGroupXML()
+    {
+        $telemetry_parser = new TelemetryParser();
+
+        $incorrect_xml_messages = array(
+            '<messagerx><sourcemsisdn>440000000000</sourcemsisdn><destinationmsisdn>440000000000</destinationmsisdn><receivedtime>25/11/2021 11:44:11</receivedtime><bearer>SMS</bearer><messageref>0</messageref><message><Content><GID>INVALID</GID><MSDN>+440000000000</MSDN><SW><SW1>1</SW1><SW2>1</SW2><SW3>0</SW3><SW4>1</SW4></SW><FN>forward</FN><TMP>34.0</TMP><KP>5</KP></Content></message></messagerx>',
+            '<messagerx><sourcemsisdn>440000000000</sourcemsisdn><destinationmsisdn>440000000000</destinationmsisdn><receivedtime>25/11/2021 11:44:11</receivedtime><bearer>SMS</bearer><messageref>0</messageref><message><Content><GID>A F</GID><MSDN>+440000000000</MSDN><SW><SW1>1</SW1><SW2>1</SW2><SW3>0</SW3><SW4>1</SW4></SW><FN>forward</FN><TMP>34.0</TMP><KP>5</KP></Content></message></messagerx>',
+            '<messagerx><sourcemsisdn>440000000000</sourcemsisdn><destinationmsisdn>440000000000</destinationmsisdn><receivedtime>25/11/2021 11:44:11</receivedtime><bearer>SMS</bearer><messageref>0</messageref><message>Invalid Message</message></messagerx>',
+            '<messagerx><sourcemsisdn>440000000000</sourcemsisdn><destinationmsisdn>440000000000</destinationmsisdn><receivedtime>25/11/2021 11:44:11</receivedtime><bearer>SMS</bearer><messageref>0</messageref><message><Content><MSDN>+440000000000</MSDN><SW><SW1>1</SW1><SW2>1</SW2><SW3>0</SW3><SW4>1</SW4></SW><FN>forward</FN><TMP>34.0</TMP><KP>5</KP></Content></message></messagerx>',
+        );
+
+        $telemetry_parser->setTelemetryMessages($incorrect_xml_messages);
+        $telemetry_parser->parseTelemetry();
+
+        $this->assertEmpty(
             $telemetry_parser->getTelemetryParseResults()
         );
     }
