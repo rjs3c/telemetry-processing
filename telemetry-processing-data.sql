dcc99820f7486f4acf1a37f08b223861c8304bea
James Brass <P2533170@my365.dmu.ac.uk>
2021-12-13 15:41:45 +0000

    Updated telemetry-processing-data.sql to match new table design.

--- telemetry-processing-data.sql
+++ telemetry-processing-data.sql
@@ -1,14 +1,16 @@
 DROP TABLE IF EXISTS `telemetry_data`;
 
 CREATE TABLE telemetry_data (
- `telemetry_id` INT COLLATE utf8_unicode_ci NOT NULL AUTO_INCREMENT PRIMARY KEY, 
- `sender_number` INT COLLATE utf8_unicode_ci NOT NULL,
- `switch_one` BIT COLLATE utf8_unicode_ci NOT NULL,
- `switch_two` BIT COLLATE utf8_unicode_ci NOT NULL,
- `switch_three` BIT COLLATE utf8_unicode_ci NOT NULL,
- `switch_four` BIT COLLATE utf8_unicode_ci NOT NULL,
- `fan` ENUM('forward', 'reverse', 'off') DEFAULT 'off' COLLATE utf8_unicode_ci NOT NULL,
- `temperature` DECIMAL(10,2) COLLATE utf8_unicode_ci NOT NULL,
- `keypad` INT(1) COLLATE utf8_unicode_ci NOT NULL,
- `timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='CURRENT_TIMESTAMP';
+`sender_number` INT COLLATE utf8_unicode_ci NOT NULL,
+`sender_group` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
+`switch_one` BIT(1) COLLATE utf8_unicode_ci NOT NULL,
+`switch_two` BIT(1) COLLATE utf8_unicode_ci NOT NULL,
+`switch_three` BIT(1) COLLATE utf8_unicode_ci NOT NULL,
+`switch_four` BIT(1) COLLATE utf8_unicode_ci NOT NULL,
+`fan` BIT(1) COLLATE utf8_unicode_ci NOT NULL,
+`temperature` DECIMAL(10,2) COLLATE utf8_unicode_ci NOT NULL,
+`keypad` INT(1) COLLATE utf8_unicode_ci NOT NULL,
+`timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON
+    UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
+COMMENT='CURRENT_TIMESTAMP';
