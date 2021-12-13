b32936a7b5f441eb7077b881f4807f1fcd4663a9
rjs3c <78426161+rjs3c@users.noreply.github.com>
2021-12-13 17:38:48 +0000

    Update telemetry-processing-data.sql

--- telemetry-processing-data.sql
+++ telemetry-processing-data.sql
@@ -1,16 +1,13 @@
 DROP TABLE IF EXISTS `telemetry_data`;
-
 CREATE TABLE telemetry_data (
-`sender_number` INT COLLATE utf8_unicode_ci NOT NULL,
-`sender_group` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
-`switch_one` BIT(1) COLLATE utf8_unicode_ci NOT NULL,
-`switch_two` BIT(1) COLLATE utf8_unicode_ci NOT NULL,
-`switch_three` BIT(1) COLLATE utf8_unicode_ci NOT NULL,
-`switch_four` BIT(1) COLLATE utf8_unicode_ci NOT NULL,
-`fan` BIT(1) COLLATE utf8_unicode_ci NOT NULL,
-`temperature` DECIMAL(10,2) COLLATE utf8_unicode_ci NOT NULL,
-`keypad` INT(1) COLLATE utf8_unicode_ci NOT NULL,
-`timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON
-    UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
-COMMENT='CURRENT_TIMESTAMP';
+ `telemetry_id` INT COLLATE utf8_unicode_ci NOT NULL AUTO_INCREMENT PRIMARY KEY,
+ `sender_number` VARCHAR(14) COLLATE utf8_unicode_ci NOT NULL,
+ `switch_one` INT(1) COLLATE utf8_unicode_ci NOT NULL,
+ `switch_two` INT(1) COLLATE utf8_unicode_ci NOT NULL,
+ `switch_three` INT(1) COLLATE utf8_unicode_ci NOT NULL,
+ `switch_four` INT(1) COLLATE utf8_unicode_ci NOT NULL,
+ `fan` ENUM('forward', 'reverse', 'off') DEFAULT 'off' COLLATE utf8_unicode_ci NOT NULL,
+ `temperature` DECIMAL(10,2) COLLATE utf8_unicode_ci NOT NULL,
+ `keypad` INT(1) COLLATE utf8_unicode_ci NOT NULL,
+ `timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='CURRENT_TIMESTAMP';
