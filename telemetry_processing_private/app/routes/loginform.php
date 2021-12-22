b545af6f10f56a14d5e8ae5edf52133834d68552
JamesB38 <78420776+JamesB38@users.noreply.github.com>
2021-12-22 19:44:39 +0000

    Added login and register functionality
    On unsuccessful login or register the title of the page is set to "unsuccessful".

--- telemetry_processing_private/app/routes/loginform.php
+++ telemetry_processing_private/app/routes/loginform.php
@@ -13,6 +13,7 @@
 
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
+use Doctrine\DBAL\DriverManager;
 
 $app->get('/loginform', function (Request $request, Response $response) use ($app)
 {
@@ -23,7 +24,7 @@ $app->get('/loginform', function (Request $request, Response $response) use ($ap
             'css_file' => CSS_PATH,
             'landing_page' => 'index.php',
             'heading_1' => 'User Login',
-            'login_action' => '/loginforn',
+            'login_action' => 'loginform',
             'links'=> array(
                 'register' => 'registerform',
                 'login' => 'loginform',
@@ -34,3 +35,70 @@ $app->get('/loginform', function (Request $request, Response $response) use ($ap
         )
     );
 })->setName('loginform');
+
+$app->post('/loginform', function (Request $request, Response $response) use ($app)
+{
+    //Get given credentials
+    $tainted_parameters = $request->getParsedBody();
+    $cleaned_parameters = cleanLoginData($app, $tainted_parameters);
+
+    //Get models
+    $container = $app->getContainer();
+    $login_model = $container->get('loginModel');
+    $bcrypt_wrapper = $container->get('bcryptWrapper');
+    $doctrine_wrapper = $container->get('doctrineWrapper');
+
+    //Doctrine wrapper setup
+    $database_connection_settings = $container->get('telemetrySettings')['doctrineSettings'];
+    $database_connection = DriverManager::getConnection($database_connection_settings);
+    $query_builder = $database_connection->createQueryBuilder();
+    $doctrine_wrapper->setQueryBuilder($query_builder);
+
+    //LoginModel setup
+    $login_model->setDoctrineWrapper($doctrine_wrapper);
+    $login_model->setBcryptWrapper($bcrypt_wrapper);
+    $login_model->setLoginCredentials($cleaned_parameters);
+
+    $login_model->login();
+    $login_result = $login_model->getLoginResult();
+
+    if($login_result)
+    {
+        return $response->withRedirect('/telemetry_processing/', 301);
+
+    }else
+    {
+        return $this->telemetryView->render($response,
+            'loginform.html.twig',
+            array(
+                'page_title' => APP_TITLE,
+                'css_file' => CSS_PATH,
+                'landing_page' => 'index.php',
+                'heading_1' => 'Unsuccessful',
+                'login_action' => 'loginform',
+                'links'=> array(
+                    'register' => 'registerform',
+                    'login' => 'loginform',
+                    'homepage' => 'index.php',
+                    'present_telemetry'=> 'presenttelemetrydata',
+                    'fetch_telemetry'=> 'fetchtelemetrydata'
+                )
+            )
+        );
+    }
+
+
+})->setName('loginform');
+
+function cleanLoginData($app, $tainted_parameters) : array {
+
+    $cleaned_parameters = [];
+
+    $authentication_validator = $app->getContainer()->get('authenticationValidator');
+
+    $cleaned_parameters['username'] = $authentication_validator->validateUserName($tainted_parameters['username']);
+    $cleaned_parameters['password'] = $tainted_parameters['password'];
+
+    return $cleaned_parameters;
+
+}
--- telemetry_processing_private/app/routes/registerform.php
+++ telemetry_processing_private/app/routes/registerform.php
@@ -13,6 +13,7 @@
 
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
+use Doctrine\DBAL\DriverManager;
 
 $app->get('/registerform', function (Request $request, Response $response) use ($app)
 {
@@ -34,3 +35,66 @@ $app->get('/registerform', function (Request $request, Response $response) use (
         )
     );
 })->setName('registerform');
+
+$app->post('/registerform', function (Request $request, Response $response) use ($app)
+{
+    //Get given credentials
+    $tainted_parameters = $request->getParsedBody();
+    $cleaned_parameters = cleanRegisterData($app, $tainted_parameters);
+
+    //Get models
+    $container = $app->getContainer();
+    $register_model = $container->get('registerModel');
+    $bcrypt_wrapper = $container->get('bcryptWrapper');
+    $doctrine_wrapper = $container->get('doctrineWrapper');
+
+    //Doctrine wrapper setup
+    $database_connection_settings = $container->get('telemetrySettings')['doctrineSettings'];
+    $database_connection = DriverManager::getConnection($database_connection_settings);
+    $query_builder = $database_connection->createQueryBuilder();
+    $doctrine_wrapper->setQueryBuilder($query_builder);
+
+    //RegisterModel setup
+    $register_model->setDoctrineWrapper($doctrine_wrapper);
+    $register_model->setBcryptWrapper($bcrypt_wrapper);
+    $register_model->setRegisterCredentials($cleaned_parameters);
+
+    $register_model->register();
+    $register_result = $register_model->getRegisterResult();
+
+
+    if($register_result) {
+        return $response->withRedirect('/telemetry_processing/', 301);
+    }else{
+        return $this->telemetryView->render($response,
+            'registerform.html.twig',
+            array(
+                'page_title' => APP_TITLE,
+                'css_file' => CSS_PATH,
+                'landing_page' => 'index.php',
+                'heading_1' => 'Unsuccessful',
+                'register_action' => 'registerform',
+                'links' => array(
+                    'register' => 'registerform',
+                    'login' => 'loginform',
+                    'homepage' => 'index.php',
+                    'present_telemetry' => 'presenttelemetrydata',
+                    'fetch_telemetry' => 'fetchtelemetrydata'
+                )
+            )
+        );
+    }
+})->setName('registerform');
+
+function cleanRegisterData($app, $tainted_parameters) : array {
+
+    $cleaned_parameters = [];
+
+    $authentication_validator = $app->getContainer()->get('authenticationValidator');
+
+    $cleaned_parameters['username'] = $authentication_validator->validateUserName($tainted_parameters['username']);
+    $cleaned_parameters['password'] = $tainted_parameters['password'];
+
+    return $cleaned_parameters;
+
+}
