a0a0d52346752176a2160932fe4ab4282822c896
JamesB38 <P2533170@my365.dmu.ac.uk>
2021-12-22 13:02:14 +0000

    Revert "Completed LoginModel and RegisterModel"
    This reverts commit 6910f21910237b3d557df41031ad98fb5f9e2107.

--- telemetry-processing-main/telemetry-processing-db.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE IF NOT EXISTS `telemetry_db` COLLATE 'utf8_unicode_ci';
-CREATE USER 'telemetry_user'@localhost IDENTIFIED BY ‘telemetry_user_pass';
-GRANT SELECT, INSERT ON telemetry_db.* TO 'telemetry_user'@'localhost';
