d9fe1fbf66fc681a363e78cbe68c4034801d33fc
JamesB38 <78420776+JamesB38@users.noreply.github.com>
2021-12-22 15:41:50 +0000

    Added telemetry-processing-users.sql

---
+++ telemetry-processing-users.sql
@@ -0,0 +1,9 @@
+USE telemetry_db;
+DROP TABLE IF EXISTS `telemetry_users`;
+CREATE TABLE telemetry_users (
+ `user_id` INT COLLATE utf8_unicode_ci NOT NULL AUTO_INCREMENT PRIMARY KEY,
+ `username` varchar(30)COLLATE utf8_unicode_ci NOT NULL,
+ `password` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+ `user_created_timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON
+UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='CURRENT_TIMESTAMP';
