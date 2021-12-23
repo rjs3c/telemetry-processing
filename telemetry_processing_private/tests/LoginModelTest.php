c0a60921b6b5d77ba0aeb62f9a36dec904f96494
rjs3c <78426161+rjs3c@users.noreply.github.com>
2021-12-23 11:21:39 +0000

    Quality Fixes to Login and Register Tests
    Added page-level doc-blocks. Added strict_types declaration to both.

--- telemetry_processing_private/tests/LoginModelTest.php
+++ telemetry_processing_private/tests/LoginModelTest.php
@@ -1,16 +1,29 @@
-<?php
-
+<?php declare(strict_types=1);
+/**
+ * LoginModelTest.php
+ *
+ * Tests Login functionality.
+ * Tests:
+ * - Tests successful authentication as a valid user.
+ * - Tests unsuccessful authentication of an invalid user.
+ *
+ * @package telemetry_processing
+ * @\TelemProc
+ *
+ * @author James Brass
+ * @author Mo Aziz
+ * @author Ryan Instrell
+ */
 
 use PHPUnit\Framework\TestCase;
 use Doctrine\DBAL\DriverManager;
 use TelemProc\DoctrineWrapper;
 use TelemProc\BcryptWrapper;
-use TelemProc\LoginModel;
-
+use TelemProc\UserLoginModel;
 
 class LoginModelTest extends TestCase
 {
-
+    /** @var array $settings Stores Bcrypt and Doctrine settings. */
     private $settings;
 
     public function __construct($name = null, array $data = [], $dataName = '')
@@ -19,66 +32,81 @@ class LoginModelTest extends TestCase
         $this->settings = require('../app/settings.php');
     }
 
+    /**
+     * @test That a user with valid credentials can be successfully authenticated.
+     *
+     * @throws \Doctrine\DBAL\Exception
+     */
     public function testLoginValidCredentials()
     {
-
-        $cleaned_parameters = array(
-
+        $test_parameters = array(
             'username' => 'testUsername',
             'password' => 'testPassword'
-
         );
 
-        //doctrine wrapper setup
         $doctrine_wrapper = new DoctrineWrapper();
+        $bcrypt_wrapper = new BcryptWrapper();
+        $login_model = new UserLoginModel();
+
         $database_connection_settings = $this->settings['telemetrySettings']['doctrineSettings'];
+        $bcrypt_settings = $this->settings['telemetrySettings']['bcryptSettings'];
+
         $database_connection = DriverManager::getConnection($database_connection_settings);
+
         $query_builder = $database_connection->createQueryBuilder();
+
         $doctrine_wrapper->setQueryBuilder($query_builder);
 
-        $bcrypt_wrapper = new BcryptWrapper();
+        $login_model->setDatabaseHandle($doctrine_wrapper);
+        $login_model->setDatabaseSettings($database_connection_settings);
 
-        $login_model = new LoginModel();
-        $login_model->setDoctrineWrapper($doctrine_wrapper);
         $login_model->setBcryptWrapper($bcrypt_wrapper);
-        $login_model->setLoginCredentials($cleaned_parameters);
+        $login_model->setBcryptSettings($bcrypt_settings);
 
-        $login_model->login();
-        $login_result = $login_model->getLoginResult();
+        $login_model->setCredentials($test_parameters);
+
+        $login_model->loginUser();
+        $login_result = $login_model->getResult();
 
         $this->assertTrue($login_result);
-
     }
 
+    /**
+     * @test That a user with invalid credentials cannot be authenticated.
+     *
+     * @throws \Doctrine\DBAL\Exception
+     */
     public function testLoginInvalidCredentials()
     {
-
-        $cleaned_parameters = array(
-
+        $test_parameters = array(
             'username' => 'invalidUsername',
             'password' => 'invalidPassword'
-
         );
 
-        //doctrine wrapper setup
         $doctrine_wrapper = new DoctrineWrapper();
+        $bcrypt_wrapper = new BcryptWrapper();
+        $login_model = new UserLoginModel();
+
         $database_connection_settings = $this->settings['telemetrySettings']['doctrineSettings'];
+        $bcrypt_settings = $this->settings['telemetrySettings']['bcryptSettings'];
+
         $database_connection = DriverManager::getConnection($database_connection_settings);
+
         $query_builder = $database_connection->createQueryBuilder();
+
         $doctrine_wrapper->setQueryBuilder($query_builder);
 
-        $bcrypt_wrapper = new BcryptWrapper();
+        $login_model->setDatabaseHandle($doctrine_wrapper);
+        $login_model->setDatabaseSettings($database_connection_settings);
 
-        $login_model = new LoginModel();
-        $login_model->setDoctrineWrapper($doctrine_wrapper);
         $login_model->setBcryptWrapper($bcrypt_wrapper);
-        $login_model->setLoginCredentials($cleaned_parameters);
+        $login_model->setBcryptSettings($bcrypt_settings);
 
-        $login_model->login();
-        $login_result = $login_model->getLoginResult();
+        $login_model->setCredentials($test_parameters);
+
+        $login_model->loginUser();
+        $login_result = $login_model->getResult();
 
         $this->assertFalse($login_result);
-
     }
-
 }
--- telemetry_processing_private/tests/RegisterModelTest.php
+++ telemetry_processing_private/tests/RegisterModelTest.php
@@ -1,15 +1,28 @@
-<?php
-
+<?php declare(strict_types=1);
+/**
+ * RegisterModelTest.php
+ *
+ * Tests Register functionality.
+ * Tests:
+ * - That a user can be successfully registered.
+ *
+ * @package telemetry_processing
+ * @\TelemProc
+ *
+ * @author James Brass
+ * @author Mo Aziz
+ * @author Ryan Instrell
+ */
 
 use PHPUnit\Framework\TestCase;
 use Doctrine\DBAL\DriverManager;
 use TelemProc\DoctrineWrapper;
 use TelemProc\BcryptWrapper;
-use TelemProc\RegisterModel;
+use TelemProc\UserRegisterModel;
 
 class RegisterModelTest extends TestCase
 {
-
+    /** @var array $settings Stores Bcrypt and Doctrine settings. */
     private $settings;
 
     public function __construct($name = null, array $data = [], $dataName = '')
@@ -19,35 +32,40 @@ class RegisterModelTest extends TestCase
     }
 
     /**
-     * Test RegisterModel. Expect to fail if run more than once.
+     * @test That a register with valid information can be successfully registered.
      *
      * @throws \Doctrine\DBAL\Exception
      */
-
     public function testRegisterUser()
     {
-        $cleaned_parameters = array(
-
+        $test_parameters = array(
             'username' => 'testUsername',
             'password' => 'testPassword'
-
         );
-        //doctrine wrapper setup
+
         $doctrine_wrapper = new DoctrineWrapper();
-        $database_connection_settings =$this->settings['telemetrySettings']['doctrineSettings'];
+        $bcrypt_wrapper = new BcryptWrapper();
+        $register_model = new UserRegisterModel();
+
+        $database_connection_settings = $this->settings['telemetrySettings']['doctrineSettings'];
+        $bcrypt_settings = $this->settings['telemetrySettings']['bcryptSettings'];
+
         $database_connection = DriverManager::getConnection($database_connection_settings);
+
         $query_builder = $database_connection->createQueryBuilder();
+
         $doctrine_wrapper->setQueryBuilder($query_builder);
 
-        $bcrypt_wrapper = new BcryptWrapper();
+        $register_model->setDatabaseHandle($doctrine_wrapper);
+        $register_model->setDatabaseSettings($database_connection_settings);
 
-        $register_model = new RegisterModel();
-        $register_model->setDoctrineWrapper($doctrine_wrapper);
         $register_model->setBcryptWrapper($bcrypt_wrapper);
-        $register_model->setRegisterCredentials($cleaned_parameters);
+        $register_model->setBcryptSettings($bcrypt_settings);
 
-        $register_model->register();
-        $register_result = $register_model->getRegisterResult();
+        $register_model->setCredentials($test_parameters);
+
+        $register_model->registerUser();
+        $register_result = $register_model->getResult();
 
         $this->assertTrue(
             $register_result
