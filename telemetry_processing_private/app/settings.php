443caefdfbe115db0cbfc573bc2fed64710c1637
rjs3c <78426161+rjs3c@users.noreply.github.com>
2021-12-23 11:22:13 +0000

    Update settings.php
    For inclusion of Bcrypt settings.

--- telemetry_processing_private/app/settings.php
+++ telemetry_processing_private/app/settings.php
@@ -36,10 +36,6 @@ define('CSS_PATH', $css_path);
 define('TEMPLATE_PATH', $template_path);
 define('LOG_PATH', $log_path);
 
-/** Constants for <Bcrypt> */
-define ('BCRYPT_ALGO', PASSWORD_DEFAULT);
-define ('BCRYPT_COST', 12);
-
 /** Settings for <Twig> (telemetryView), <PDO> (databaseSettings) and <SOAPClient> (soapSettings). **/
 return array(
     'telemetrySettings' => array(
