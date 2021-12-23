3e541aa4598db35e0b0bcd1f7831e0f9ebd790d2
rjs3c <78426161+rjs3c@users.noreply.github.com>
2021-12-23 11:51:14 +0000

    Add files via upload

--- telemetry_processing_private/app/routes/registerform.php
+++ telemetry_processing_private/app/routes/registerform.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * registerform.php
+ * register.php
  *
  * Displays the form necessary for users to register their information.
  *
@@ -13,8 +13,9 @@
 
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
+use Doctrine\DBAL\DriverManager;
 
-$app->get('/registerform', function (Request $request, Response $response) use ($app) : Response
+$app->get('/registerform', function (Request $request, Response $response) use ($app)
 {
     return $this->telemetryView->render($response,
         'registerform.html.twig',
@@ -23,7 +24,7 @@ $app->get('/registerform', function (Request $request, Response $response) use (
             'css_file' => CSS_PATH,
             'landing_page' => 'index.php',
             'heading_1' => 'User Register',
-            'register_action' => 'register',
+            'register_action' => 'registerform',
             'links'=> array(
                 'register' => 'registerform',
                 'login' => 'loginform',
@@ -34,3 +35,66 @@ $app->get('/registerform', function (Request $request, Response $response) use (
         )
     );
 })->setName('registerform');
+
+$app->post('/registerform', function (Request $request, Response $response) use ($app)
+{
+    //Get given credentials
+    $tainted_parameters = $request->getParsedBody();
+    $cleaned_parameters = cleanRegisterData($app, $tainted_parameters);
+
+    //Get models
+    $container = $app->getContainer();
+    $register_model = $container->get('registerModel');
+    $bcrypt_wrapper = $container->get('bcryptWrapper');
+    $doctrine_wrapper = $container->get('doctrineWrapper');
+
+    //Doctrine wrapper setup
+    $database_connection_settings = $container->get('telemetrySettings')['doctrineSettings'];
+    $database_connection = DriverManager::getConnection($database_connection_settings);
+    $query_builder = $database_connection->createQueryBuilder();
+    $doctrine_wrapper->setQueryBuilder($query_builder);
+
+    //RegisterModel setup
+    $register_model->setDoctrineWrapper($doctrine_wrapper);
+    $register_model->setBcryptWrapper($bcrypt_wrapper);
+    $register_model->setRegisterCredentials($cleaned_parameters);
+
+    $register_model->register();
+    $register_result = $register_model->getRegisterResult();
+
+
+    if($register_result) {
+        return $response->withRedirect('/telemetry_processing/', 301);
+    }else{
+        return $this->telemetryView->render($response,
+            'registerform.html.twig',
+            array(
+                'page_title' => APP_TITLE,
+                'css_file' => CSS_PATH,
+                'landing_page' => 'index.php',
+                'heading_1' => 'Unsuccessful',
+                'register_action' => 'registerform',
+                'links' => array(
+                    'register' => 'registerform',
+                    'login' => 'loginform',
+                    'homepage' => 'index.php',
+                    'present_telemetry' => 'presenttelemetrydata',
+                    'fetch_telemetry' => 'fetchtelemetrydata'
+                )
+            )
+        );
+    }
+})->setName('registerform');
+
+function cleanRegisterData($app, $tainted_parameters) : array {
+
+    $cleaned_parameters = [];
+
+    $authentication_validator = $app->getContainer()->get('authenticationValidator');
+
+    $cleaned_parameters['username'] = $authentication_validator->validateUserName($tainted_parameters['username']);
+    $cleaned_parameters['password'] = $tainted_parameters['password'];
+
+    return $cleaned_parameters;
+
+}
