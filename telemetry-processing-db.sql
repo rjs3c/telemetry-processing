979bf03d924c3847e8440e7a9d92b5507764b8c7
rjs3c <78426161+rjs3c@users.noreply.github.com>
2022-01-01 20:23:31 +0000

    Update telemetry-processing-db.sql
    Broken ` syntax fix.

--- telemetry-processing-db.sql
+++ telemetry-processing-db.sql
@@ -1,3 +1,3 @@
 CREATE DATABASE IF NOT EXISTS `telemetry_db` COLLATE 'utf8_unicode_ci';
-CREATE USER 'telemetry_user'@localhost IDENTIFIED BY ‘telemetry_user_pass';
-GRANT SELECT, INSERT ON telemetry_db.* TO 'telemetry_user'@'localhost';
+CREATE USER 'telemetry_user'@localhost IDENTIFIED BY 'telemetry_user_pass';
+GRANT SELECT, INSERT, DELETE ON telemetry_db.* TO 'telemetry_user'@'localhost';
