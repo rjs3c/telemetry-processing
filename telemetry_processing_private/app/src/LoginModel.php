967f5342b9e879191d0eb3d252e9ff0e283f254a
rjs3c <78426161+rjs3c@users.noreply.github.com>
2022-01-04 11:26:53 +0000

    Updated Login/Register (Sessions/Logging)
    Incorporated Mo's Sessions Additions to LoginModel using SessionWrapper.
    Added functionality to log upon successful login and registration.

--- telemetry_processing_private/app/src/LoginModel.php
+++ telemetry_processing_private/app/src/LoginModel.php
@@ -16,80 +16,146 @@ namespace TelemProc;
 
 class LoginModel
 {
-
-    private $doctrine_wrapper;
+    /** @var resource $bcrypt_wrapper Contains the handle to <Bcrypt>. */
     private $bcrypt_wrapper;
+
+    /** @var resource $doctrine_wrapper Contains the handle to <Doctrine>.*/
+    private $doctrine_wrapper;
+
+    /** @var resource $logger_handle Contains handle to <Monolog>. */
+    private $logger_handle;
+
+    /** @var resource $session_wrapper Contains the handle to <SessionWrapper>. */
+    private $session_wrapper;
+
+    /** @var array $login_credentials Contains user-entered credentials. */
     private $login_credentials;
+
+    /** @var bool $login_result Contains the result from authentication. */
     private $login_result;
 
     public function __construct()
     {
+        $this->bcrypt_wrapper = null;
+        $this->doctrine_wrapper = null;
+        $this->logger_handle = null;
+        $this->login_credentials = array();
+        $this->login_result = false;
     }
 
-    public function __destruct()
-    {
-        // TODO: Implement __destruct() method.
-    }
-
-    /**
-     * Sets <Doctrine> wrapper
-     *
-     * @param $doctrine_wrapper
-     */
-    public function setDoctrineWrapper($doctrine_wrapper)
-    {
-        $this->doctrine_wrapper = $doctrine_wrapper;
-    }
+    public function __destruct() {}
 
     /**
      * Sets <Bcrypt> wrapper
      *
      * @param $bcrypt_wrapper
      */
-    public function setBcryptWrapper($bcrypt_wrapper)
+    public function setBcryptWrapper($bcrypt_wrapper) : void
     {
         $this->bcrypt_wrapper = $bcrypt_wrapper;
     }
 
     /**
+     * Sets <DoctrineWrapper> handle.
+     *
+     * @param $doctrine_wrapper
+     */
+    public function setDoctrineWrapper($doctrine_wrapper) : void
+    {
+        $this->doctrine_wrapper = $doctrine_wrapper;
+    }
+
+    /**
+     * Sets <Monolog> handle.
+     *
+     * @param $logger_handle
+     */
+    public function setLoggerHandle($logger_handle) : void
+    {
+        $this->logger_handle = $logger_handle;
+    }
+
+    /**
+     * Sets <SessiomnWrapper>.
+     *
+     * @param $session_wrapper
+     */
+    public function setSessionWrapper($session_wrapper) : void
+    {
+        $this->session_wrapper = $session_wrapper;
+    }
+
+    /**
      * Sets login credentials
      *
-     * @param $login_credentials
+     * @param array $login_credentials
      */
-    public function setLoginCredentials($login_credentials)
+    public function setLoginCredentials(array $login_credentials) : void
     {
         $this->login_credentials = $login_credentials;
     }
 
+    /**
+     * Returns result for user authentication.
+     *
+     * @return bool
+     */
     public function getLoginResult() : bool
     {
         return $this->login_result;
     }
 
     /**
+     * Using the Logger handle, produce a log of level NOTICE
+     * Optional parameters in $additional if more information is needed.
+     *
+     * @param string $log_message
+     * @param array|null $additional
+     */
+    private function logEvent(string $log_message, ?array $additional = null) : void
+    {
+        if ($additional !== null) {
+            $this->logger_handle->notice($log_message, $additional);
+        } else {
+            $this->logger_handle->notice($log_message);
+        }
+    }
+
+    /**
      * Logs in a user. Returns true if logged in successfully and false otherwise.
      *
-     * @return bool
+     * @return void
      */
     public function login() : void
     {
         $login_result = false;
 
-        //fetch password for given username
+        // Fetch password for given username
         $username = $this->login_credentials['username'];
-        if(!empty($username)) {
+
+        if (!empty($username)) {
             $this->doctrine_wrapper->fetchUserPassword($username);
             $query_result = $this->doctrine_wrapper->getQueryResult();
 
             if (!empty($query_result)) {
                 $fetched_password = $query_result[0]['password'];
 
-                //check if password is equal to given password
+                // Check if password is equal to given password
                 $given_password = $this->login_credentials['password'];
                 $login_result = $this->bcrypt_wrapper->authenticatePassword($given_password, $fetched_password);
             }
         }
+
+        if ($login_result !== false) {
+            // Adding to Session Var
+            $this->session_wrapper->setSessionVar('user', $this->login_credentials['username']);
+
+            // Log Successful Authentication
+            if ($this->logger_handle !== null) {
+                $this->logEvent('User Authentication', array($this->login_credentials['username']));
+            }
+        }
+
         $this->login_result = $login_result;
     }
-
 }
--- telemetry_processing_private/app/src/RegisterModel.php
+++ telemetry_processing_private/app/src/RegisterModel.php
@@ -14,22 +14,42 @@
 
 namespace TelemProc;
 
-
 class RegisterModel
 {
-
-    private $doctrine_wrapper;
+    /** @var resource $bcrypt_wrapper Contains the handle to <Bcrypt>. */
     private $bcrypt_wrapper;
+
+    /** @var resource $doctrine_wrapper Contains the handle to <Doctrine>.*/
+    private $doctrine_wrapper;
+
+    /** @var resource $logger_handle Contains handle to <Monolog>. */
+    private $logger_handle;
+
+    /** @var array $register_credentials Contains user-entered credentials. */
     private $register_credentials;
+
+    /** @var bool $register_result Contains the result from registration. */
     private $register_result;
 
     public function __construct()
     {
+        $this->bcrypt_wrapper = null;
+        $this->doctrine_wrapper = null;
+        $this->logger_handle = null;
+        $this->register_credentials = array();
+        $this->register_result = false;
     }
 
-    public function __destruct()
+    public function __destruct() {}
+
+    /**
+     * Sets <Bcrypt> wrapper
+     *
+     * @param $bcrypt_wrapper
+     */
+    public function setBcryptWrapper($bcrypt_wrapper) : void
     {
-        // TODO: Implement __destruct() method.
+        $this->bcrypt_wrapper = $bcrypt_wrapper;
     }
 
     /**
@@ -37,65 +57,91 @@ class RegisterModel
      *
      * @param $doctrine_wrapper
      */
-    public function setDoctrineWrapper($doctrine_wrapper)
+    public function setDoctrineWrapper($doctrine_wrapper) : void
     {
         $this->doctrine_wrapper = $doctrine_wrapper;
     }
 
     /**
-     * Sets <Bcrypt> wrapper
+     * Sets <Monolog> handle.
      *
-     * @param $bcrypt_wrapper
+     * @param $logger_handle
      */
-    public function setBcryptWrapper($bcrypt_wrapper)
+    public function setLoggerHandle($logger_handle) : void
     {
-        $this->bcrypt_wrapper = $bcrypt_wrapper;
+        $this->logger_handle = $logger_handle;
     }
 
     /**
      * Sets register credentials
      *
-     * @param $register_credentials
+     * @param array $register_credentials
      */
-    public function setRegisterCredentials($register_credentials)
+    public function setRegisterCredentials(array $register_credentials) : void
     {
         $this->register_credentials = $register_credentials;
     }
 
+    /**
+     * Returns result for user registration.
+     *
+     * @return bool
+     */
     public function getRegisterResult() : bool
     {
         return $this->register_result;
     }
 
     /**
+     * Using the Logger handle, produce a log of level NOTICE
+     * Optional parameters in $additional if more information is needed.
+     *
+     * @param string $log_message
+     * @param array|null $additional
+     */
+    private function logEvent(string $log_message, ?array $additional = null) : void
+    {
+        if ($additional !== null) {
+            $this->logger_handle->notice($log_message, $additional);
+        } else {
+            $this->logger_handle->notice($log_message);
+        }
+    }
+
+    /**
      * Registers new users. Returns true if registered successfully and false if not.
      * Will not allow multiple accounts of the same username and will return false if attempted.
-     *
-     * @return bool
      */
     public function register() : void
     {
         $register_result = false;
 
-        //Check if account with username already exists
+        // Check if account with username already exists
         $username = $this->register_credentials['username'];
-        if(!empty($username)) {
-            $this->doctrine_wrapper->checkIfUsernameAvailable($username);
-            $isAvailable = $this->doctrine_wrapper->getQueryResult();
 
-            //Hash password
-            if ($isAvailable) {
+        if (!empty($username)) {
+            $this->doctrine_wrapper->checkIfUsernameAvailable($username);
+            $is_available = $this->doctrine_wrapper->getQueryResult();
+
+            // Hash password
+            if ($is_available) {
                 $password = $this->register_credentials['password'];
                 $hashed_password = $this->bcrypt_wrapper->createHashedPassword($password);
 
-                //Store new user details
-                if(!empty($hashed_password)) { //if hash of password hasn't failed
+                // Store new user details
+                if (!empty($hashed_password)) { // If hash of password hasn't failed
                     $this->doctrine_wrapper->storeUserDetails($username, $hashed_password);
                     $register_result = $this->doctrine_wrapper->getQueryResult();
                 }
             }
         }
+
+        // Log Successful Registration
+        if ($register_result !== false
+            && $this->logger_handle !== null) {
+                $this->logEvent('User Registration', array($this->register_credentials['username']));
+        }
+
         $this->register_result = $register_result;
     }
-
 }
