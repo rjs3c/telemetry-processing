f76847c1ee531b051d03fd7364cdebc25a8579d4
rjs3c <78426161+rjs3c@users.noreply.github.com>
2022-01-04 10:53:38 +0000

    Added Mo's SessionWrapper.php
    PSR-12 spot-checks, ensured consistent white-spaces, curly-braces. Updated top-level docblock to include a description.
    FYI the mixed return type is PHP 8.0 only, doesn't work on the standard PHP 7.4.

--- telemetry_processing_private/app/src/SessionWrapper.php
+++ telemetry_processing_private/app/src/SessionWrapper.php
@@ -1,10 +1,75 @@
 <?php
-
+/**
+ * SessionWrapper.php
+ *
+ * Provides a wrapper for session management, variable setting and unsetting.
+ *
+ * @package telemetry_processing
+ * @\TelemProc
+ *
+ * @author James Brass
+ * @author Mo Aziz
+ * @author Ryan Instrell
+ */
 
 namespace TelemProc;
 
-
 class SessionWrapper
 {
+    public function __construct() {}
 
+    public function __destruct() {}
+
+    /**
+     * Sets session variable
+     * @param string $session_var
+     * @param string $session_value
+     * @return bool
+     */
+    public function setSessionVar(string $session_var, string $session_value) : bool
+    {
+        $session_set_result = false;
+
+        if (isset($session_var) && !empty($session_value)) {
+            $_SESSION[$session_var] = $session_value;
+            $session_set_result = true;
+        }
+
+        return $session_set_result;
+    }
+
+    /**
+     * Gets session variable with the correct key
+     * @param string $session_var
+     * @return mixed
+     */
+    public function getSessionVar(string $session_var)
+    {
+        $returned_var = null;
+
+        if (isset($_SESSION[$session_var])) {
+            $returned_var = $_SESSION[$session_var];
+        }
+
+        return $returned_var;
+    }
+
+    /**
+     * Unsets the session variable
+     * @param string $session_var
+     * @return bool
+     */
+    public function unsetSessionVar(string $session_var) : bool
+    {
+        $session_unset_result = false;
+
+        if (isset($_SESSION[$session_var])) {
+            unset($_SESSION[$session_var]);
+            if (!isset($_SESSION[$session_var])) {
+                $session_unset_result = true;
+            }
+        }
+
+        return $session_unset_result;
+    }
 }
