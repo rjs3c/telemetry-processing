668923c6c899319b53eb8f25e214307f22e31e36
rjs3c <78426161+rjs3c@users.noreply.github.com>
2022-01-04 11:18:45 +0000

    Added Mo's SessionWrapperTest and Spot-checks
    Made PSR-12 spot-checks, top-level docblock is consistent with other tests. Added strict_types declaration to match other tests.
    The inclusion of settings.php is not needed here, as no settings required to get SessionWrapper to work.

---
+++ telemetry_processing_private/tests/SessionWrapperTest.php
@@ -0,0 +1,59 @@
+<?php declare(strict_types=1);
+/**
+ * SessionWrapperTest.php
+ *
+ * Tests session management using the SessionWrapper.
+ * Tests:
+ * - Successful setting of a session variable.
+ * - Successful retrieval of previously set session variable.
+ * - Successful unsetting of previously set session variable.
+ *
+ * @package telemetry_processing
+ * @\TelemProc
+ *
+ * @author James Brass
+ * @author Mo Aziz
+ * @author Ryan Instrell
+ */
+
+use PHPUnit\Framework\TestCase;
+use TelemProc\SessionWrapper;
+
+class SessionWrapperTest extends TestCase
+{
+    /**
+     * @test For storing data. Expected output is true.
+     * Fails if value isn't found in $_SESSION.
+     */
+    public function testSetSessionVar()
+    {
+        $session_wrapper = new SessionWrapper();
+
+        $result = $session_wrapper->setSessionVar('user', 'username');
+        $this->assertTrue($result);
+    }
+
+    /**
+     * @test For retrieving data. Expected output is 'username'.
+     * Fails if value doesn't equal the one stored in $_SESSION.
+     */
+    public function testGetSessionVar()
+    {
+        $session_wrapper = new SessionWrapper();
+
+        $result = $session_wrapper->getSessionVar('user');
+        $this->assertEquals('username', $result);
+    }
+
+    /**
+     * @test For unsetting session data. Expected output is true.
+     * Fails if data in $_SESSION cannot be unset.
+     */
+    public function testUnsetSessionVar()
+    {
+        $session_wrapper = new SessionWrapper();
+
+        $result = $session_wrapper->unsetSessionVar('user');
+        $this->assertTrue($result);
+    }
+}
