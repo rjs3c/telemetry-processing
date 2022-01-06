e11020ca0d355b7606ff34a7f40a5192942be0d6
rjs3c <78426161+rjs3c@users.noreply.github.com>
2022-01-06 16:26:29 +0000

    Add Dummy Data for Viva

--- telemetry-processing-users.sql
+++ telemetry-processing-users.sql
@@ -8,3 +8,8 @@ CREATE TABLE telemetry_users (
  `password` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
  `user_created_timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='CURRENT_TIMESTAMP';
+
+--
+-- Telemetry Users Dummy Data
+--
+INSERT INTO `telemetry_users` VALUES ('1', 'telemetry_user', '$2y$12$DLBlHC2tqH3716bl3qIJEOj/YC9rtRXkVOltx4QA9LvDoj1ESVVpC', '2022-01-01 00:00:00'); -- password: telemetry_user_pass
