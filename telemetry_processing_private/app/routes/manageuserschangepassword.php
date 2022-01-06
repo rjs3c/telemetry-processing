952cd33fb718aca177f306fbf43fbf759d889795
rjs3c <78426161+rjs3c@users.noreply.github.com>
2022-01-06 13:32:37 +0000

    Update manageuserschangepassword (PSR-12)
    Added missing whitespace in callback.

--- telemetry_processing_private/app/routes/manageuserschangepassword.php
+++ telemetry_processing_private/app/routes/manageuserschangepassword.php
@@ -14,7 +14,7 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-$app->post('/manageuserschangepassword', function(Request $request, Response $response) use ($app) : Response
+$app->post('/manageuserschangepassword', function (Request $request, Response $response) use ($app) : Response
 {
     $tainted_parameters = $request->getParsedBody();
 
