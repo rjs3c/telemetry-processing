f770cac0f53b8713e625dfc081e7c3b2fa58256c
rjs3c <78426161+rjs3c@users.noreply.github.com>
2022-01-06 08:38:31 +0000

    Update telemetry-processing-db.sql (Comments)
    Add '--' comments.

--- telemetry-processing-db.sql
+++ telemetry-processing-db.sql
@@ -1,3 +1,6 @@
+--
+-- Telemetry Database
+--
 CREATE DATABASE IF NOT EXISTS `telemetry_db` COLLATE 'utf8_unicode_ci';
 CREATE USER 'telemetry_user'@localhost IDENTIFIED BY 'telemetry_user_pass';
 GRANT SELECT, INSERT, UPDATE, DELETE ON telemetry_db.* TO 'telemetry_user'@'localhost';
