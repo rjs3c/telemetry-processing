20c6a1cdefc307beb6821aec41bd326aa204c3dd
rjs3c <78426161+rjs3c@users.noreply.github.com>
2022-01-08 10:58:18 +0000

    Update telemetry-processing-data.sql (Dummy Data)
    As per the coursework spec, added some dummy data.

--- telemetry-processing-data.sql
+++ telemetry-processing-data.sql
@@ -14,3 +14,11 @@ CREATE TABLE telemetry_data (
  `keypad` INT(1) COLLATE utf8_unicode_ci NOT NULL,
  `timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='CURRENT_TIMESTAMP';
+
+--
+-- Telemetry Dummy Data
+--
+INSERT INTO `telemetry_data` VALUES ('1', '+440000000000', 1, 1, 1, 1, 'forward', 34.0, 1, '2022-01-01 00:00:00');
+INSERT INTO `telemetry_data` VALUES ('2', '+440000000000', 0, 0, 0, 0, 'reverse', 23.0, 5, '2022-01-01 00:00:00');
+INSERT INTO `telemetry_data` VALUES ('3', '+440000000000', 1, 0, 1, 0, 'off', 28.0, 9, '2022-01-01 00:00:00');
+INSERT INTO `telemetry_data` VALUES ('4', '+440000000000', 0, 1, 0, 1, 'off', 31.0, 2, '2022-01-01 00:00:00');
