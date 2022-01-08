7e4c797f5b713269bf5b5c2c29c5c0459a0948fd
rjs3c <78426161+rjs3c@users.noreply.github.com>
2022-01-08 10:57:11 +0000

    Update telemetry-processing-users.sql (Admin)
    Add 'admin' field to script - as well as default admin user.

--- telemetry-processing-users.sql
+++ telemetry-processing-users.sql
@@ -4,7 +4,13 @@
 DROP TABLE IF EXISTS `telemetry_users`;
 CREATE TABLE telemetry_users (
  `id` INT COLLATE utf8_unicode_ci NOT NULL AUTO_INCREMENT PRIMARY KEY,
+ `admin` INT(2) COLLATE utf8_unicode_ci NOT NULL DEFAULT 0,
  `username` varchar(30)COLLATE utf8_unicode_ci NOT NULL,
  `password` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
  `user_created_timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='CURRENT_TIMESTAMP';
+
+--
+-- Telemetry Users Administrator. Password: telemetry_user_pass
+--
+INSERT INTO `telemetry_users` VALUES ('1', '1', 'telemetry_user', '$2y$12$DLBlHC2tqH3716bl3qIJEOj/YC9rtRXkVOltx4QA9LvDoj1ESVVpC', '2022-01-01 00:00:00');
